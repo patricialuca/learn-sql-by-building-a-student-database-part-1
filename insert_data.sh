cce4f2516c93a8cd7f1470b40c9c5b0725b8503c
Patricia Luengo Carretero <patricialuengocarretero@gmail.com>
2024-03-28 11:02:46 +0000

    Save progress: 1033.1

--- insert_data.sh
+++ insert_data.sh
@@ -57,7 +57,8 @@ do
 if [[ $FIRST != "first_name" ]]
   then
     #get major_id
-       #MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    echo $MAJOR_ID
     #if not found
     #set to null
     #insert student
